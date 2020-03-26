a9e8e6554a53c48213989dfadecf42206537a2bf
Chmouel Boudjnah <chmouel@redhat.com>
2020-03-26 09:13:52 +0100

    Properly order pipeline-latest-release even with RC
    and increase MAX_SHIFT by a lot since there could be a lot of RCs

--- openshift/pipeline-latest-release.sh
+++ openshift/pipeline-latest-release.sh
@@ -6,13 +6,13 @@
 # If not it exit 1
 # It can take the argument --only-stable-release to not do nightly but only detect the pipeline version
 
-MAX_SHIFT=2
+MAX_SHIFT=${MAX_SHIFT:-5}
 NIGHTLY_RELEASE="https://raw.githubusercontent.com/openshift/tektoncd-pipeline/release-next/openshift/release/tektoncd-pipeline-nightly.yaml"
 STABLE_RELEASE_URL='https://raw.githubusercontent.com/openshift/tektoncd-pipeline/${version}/openshift/release/tektoncd-pipeline-${version}.yaml'
 
 function get_version {
     local shift=${1} # 0 is latest, increase is the version before etc...
-    local version=$(curl -s https://api.github.com/repos/tektoncd/pipeline/releases | python -c "import sys, json;x=json.load(sys.stdin);print(x[${shift}]['tag_name'])")
+    local version=$(curl -s https://api.github.com/repos/tektoncd/pipeline/releases| python -c "from distutils.version import LooseVersion;import sys, json;jeez=json.load(sys.stdin);print(sorted([x['tag_name'] for x in jeez], key=LooseVersion, reverse=True)[${shift}])")
     echo $(eval echo ${STABLE_RELEASE_URL})
 }
 
